c90f378e4f525be6c38665b46d6c6bd461807666
aopaixao <aopaixao@outlook.com>
2021-06-07 12:56:56 -0300

    Update SpringFoxConfig.java

--- swagger/SpringFoxConfig.java
+++ swagger/SpringFoxConfig.java
@@ -2,8 +2,11 @@ package com.residencia.dell.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
@@ -12,9 +15,21 @@ public class SpringFoxConfig {
 	@Bean
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
+          .apiInfo(apiInfo())
           .select()                                  
           .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
+          .paths(PathSelectors.any()) 
           .build();                                           
     }
+	
+	private ApiInfo apiInfo() {
+		ApiInfo apiInfo = new ApiInfoBuilder()
+				.title ("Documentação da API DellStore")
+				.description ("Essa API foi desenvolvida para estudo do Framework Spring")
+				.license ("Apache License Version 2.0")
+				.version ("1.0.0")
+				.build();
+		
+		return apiInfo;
+	}
 }
